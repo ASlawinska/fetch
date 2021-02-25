56db049fbb8e16d2eeeb0f2c43e907bd5fefa2ea
ASlawinska <slawinska.adrianna@gmail.com>
2021-02-25 23:58:51 +0100

    Added fetch funcion and currency calculator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import style from "./css/scss/index.scss";
+document.getElementById("select").addEventListener("change",calc)
 
-console.log("Hello world");
+function calc(){
+    let inputValue = document.getElementById("CCY").value
+
+    if (inputValue <= 0) {
+        document.getElementById("output").innerText = "wprowadzono zla liczbe"
+    } else { 
+        fetch(`https://api.nbp.pl/api/exchangerates/rates/c/${this.value}/?format=json`)
+        .then((resp)=>resp.json())
+        .then(function(data){ 
+           document.getElementById("output").innerHTML = `po przeliczeniu kwota to: ${data.rates[0].ask * inputValue} PLN`
+        })
+    }
+};
